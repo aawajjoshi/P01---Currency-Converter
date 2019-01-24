21daa53bce316b16d3c305a09e3b73b1e8412f8f
aawajjoshi <s521315@nwmissouri.edu>
2019-01-24 16:35:53 -0600

    Made final changes.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace p01
 {
@@ -8,15 +9,28 @@ namespace p01
         {
             Console.WriteLine("Currency Converter");
             Console.WriteLine("------------------- \n");
+
+            //Getting user's info
+            var name = "";
+            var age = 0;
+
+            Console.Write("Please enter your first name: ");
+            name = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.Write($"{name}, please enter your age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
             //Declaring currencies and initializing their current values
             var euro = 0.88;
-            var yen = 109.78;
-            var yuan = 6.78;
-            var nrs = 113.65;
-
-            //var newamount = 0; //var is strongly-typed and not dynamically-typed
-
+            var yen = 109.61;
+            var yuan = 6.79;
+            var nrs = 113.43;
+            var peso = 19.02;
             var usd = 0.0;
+
+            //var is strongly-typed and not dynamically-typed
 
             var canConvert = false;
             //var exit = 'q';
@@ -24,24 +38,26 @@ namespace p01
 
             while (!canConvert)
             {
-                //Asking the user for a usd value
+
                 Console.Write("Enter an amount in USD to convert and press Enter: ");
                 usd = Convert.ToDouble(Console.ReadLine());
                 Console.WriteLine();
 
                 //Asking the user the currency they want top convert to
                 Console.WriteLine("Choose the currency you want to convert to or press q to quit: ");
-                
-                Console.WriteLine("\t eu - Euro"); //(€)");
-                Console.WriteLine("‎\t ye - Yen"); //(¥)‎");
-                Console.WriteLine("‎\t yu - Yuan"); //(元)‎");
-                Console.WriteLine("‎\t nr - Nepalese Rupees"); //(Rs.)");
-                Console.WriteLine("‎");
-        
+                Console.WriteLine("\t ------------------");
+                Console.WriteLine("\t eu - Euro"); 
+                Console.WriteLine("‎\t ye - Japanese Yen"); 
+                Console.WriteLine("‎\t yu - Chinese Yuan"); 
+                Console.WriteLine("‎\t nr - Nepalese Rupee"); 
+                Console.WriteLine("\t ps - Mexican Peso");
+                Console.WriteLine("\t ------------------");
                 var str = Console.ReadLine();
+                Console.WriteLine();
+                
                 if (str == "q")
                 {
-                    Console.WriteLine("Safe travels!");
+                    Console.WriteLine($"Thank you for using our services {name} and safe travels!\n");
                     break;
                 }
 
@@ -51,22 +67,27 @@ namespace p01
                     switch (str)
                     {
                         case "eu":
-                            Console.WriteLine($"${usd} in Euros is: " + (usd * euro) + " euros");
+                            Console.WriteLine($"${usd} in Euros is " + (usd * euro) + " euros\n");
                             break;
 
                         case "ye":
-                            Console.WriteLine($"${usd} in Yen is: " + (usd * yen) + " yen");
+                            Console.WriteLine($"${usd} in Japanese Yen is " + (usd * yen) + " yen\n");
                             break;
 
                         case "yu":
-                            Console.WriteLine($"${usd} in Yuan is: " + (usd * yuan) + " yuan");
+                            Console.WriteLine($"${usd} in Chinese Yuan is " + (usd * yuan) + " yuan\n");
                             break;
 
                         case "nr":
-                            Console.WriteLine($"${usd} in Nrs is: " + (usd * nrs) + " rupees");
+                            Console.WriteLine($"${usd} in Nepalese Rupee is " + (usd * nrs) + " rupees\n");
+                            break;
+
+                        case "ps":
+                            Console.WriteLine($"${usd} in Mexican Peso is: " + (usd * peso) + " pesos\n");
                             break;
 
                         default:
+                            Console.WriteLine();
                             break;
                     }
                 }
